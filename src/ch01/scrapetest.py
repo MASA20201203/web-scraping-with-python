df98499d2f7b08f85b67a0840ce605255c03a4d1
MASA20201213 <koedame032@gmail.com>
2025-01-07 12:44:07 +0900

    update: ch01 BeautifulSoup の使い方に修正

--- src/ch01/scrapetest.py
+++ src/ch01/scrapetest.py
@@ -5,5 +5,5 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 html = urlopen("http://www.pythonscraping.com/pages/page1.html")
-bs = BeautifulSoup(html.read(), "html.parser")
+bs = BeautifulSoup(html, "html.parser")
 print(bs.h1)
